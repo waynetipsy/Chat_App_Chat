3fa0704527078f37c0719bd8637ad06c1d794718
Ifeanyi Etoniru <jacketoniru@gmail.com>
2023-10-21 20:42:44 +0100

    Drawer

--- lib/components/wall_post.dart
+++ lib/components/wall_post.dart
@@ -1,15 +1,62 @@
 import 'package:flutter/material.dart';
+import '../components/like_button.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-class WallPost extends StatelessWidget {
+
+class WallPost extends StatefulWidget {
   final String message;
   final String user;
   //final String time;
+  final String postId;
+  final List<String> likes;
   const WallPost({
     super.key,
     required  this.message,
     required  this.user,
     //required  this.time,
+     required  this.postId,
+    required this.likes
     });
+
+  @override
+  State<WallPost> createState() => _WallPostState();
+}
+
+class _WallPostState extends State<WallPost> {
+  // user
+  final currentUser = FirebaseAuth.instance.currentUser!;
+   bool isLiked = false;
+
+   @override
+  void initState() {
+    super.initState();
+    isLiked = widget.likes.contains(currentUser.email);
+  }
+
+  // toggle like
+  void toggleLike() {
+    setState(() {
+      isLiked = !isLiked;
+    });
+
+    // Access the document in Firebase
+    DocumentReference postRef = FirebaseFirestore.instance.
+    collection('User Posts').doc(widget.postId);
+
+    if (isLiked) {
+  // if the post is now liked, add the user's email to the 'likes' field
+    postRef.update({
+      'Likes' : FieldValue.arrayUnion([currentUser.email])
+    });
+    } else {
+      // if the post is now unliked, remove the user's email from the 'Likes' field
+     postRef.update({
+      'Likes' : FieldValue.arrayRemove([currentUser.email])
+     });
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -22,8 +69,29 @@ class WallPost extends StatelessWidget {
       padding: const EdgeInsets.all(25),
       child: Row(
         children: [
+
+        Column(
+          children: [
+            // like button
+            LikeButton(
+              isLiked: isLiked,
+              onTap: toggleLike,
+            ),
+
+           const SizedBox(height: 5),
+
+            // like count
+            Text(widget.likes.length.toString(),
+              style: const TextStyle(color:Colors.grey,
+              ),
+            ),
+            
+          ],
+        ),
+         const SizedBox(width: 20,),
+
           // profile pic
-          Container(
+         /* Container(
             decoration: BoxDecoration(
               shape: BoxShape.circle,
               color: Colors.grey[400]
@@ -32,16 +100,19 @@ class WallPost extends StatelessWidget {
             child: const Icon(Icons.person,
                color: Colors.white,
             ),
-          ),
-          
+          ), 
+             */
+
+
+          // message and user email
           Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              Text(user, style: TextStyle(color: Colors.grey[500]),
+              Text(widget.user, style: TextStyle(color: Colors.grey[500]),
               
               ),
                  const SizedBox(height: 10,),
-              Text(message),
+              Text(widget.message),
             ],
           )
         ],
